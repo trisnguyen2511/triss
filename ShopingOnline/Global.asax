bcc80919cd7da439b760496c50be195b5e31e0c5
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2021-06-11 11:15:21 +0700

    ShopOnline

---
+++ ShopingOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopingOnline.MvcApplication" Language="C#" %>
---
+++ ShopingOnline/Models/BUS/ShopOnlineBus.cs
@@ -0,0 +1,25 @@
+﻿using QLMobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopingOnline.Models.BUS
+{
+    public class ShopOnlineBus
+    {
+        public static IEnumerable<Product> DanhSach()
+        {
+            var db = new QLMobileShopConnectionDB();
+            return db.Query<Product>("select * from Products where ProductStatus = 1");
+        }
+
+        public static Product Detail(String a)
+        {
+            var db = new QLMobileShopConnectionDB();
+            string s = "select * from Products where ProductsID = '" + a + "'";
+            return db.SingleOrDefault<Product> (s);
+        }
+
+    }
+}
